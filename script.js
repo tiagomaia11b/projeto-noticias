3b813744c6d799c3d3902bc99d9afdacc0c52165
tmaia5003 <tmaia5003@gmail.com>
2025-06-01 02:34:39 +0000

    Criação da navegação desktop, remoção da classe header-mobile e inicio da responsividade

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const botaoabrir = document.querySelector(".header-mobile > button ")
-const botaofechar = document.querySelector(".header-mobile nav button")
+const botaoabrir = document.querySelector("header > button ")
+const botaofechar = document.querySelector("header nav button")
 
 const body = document.querySelector("body")
-const nav = document.querySelector(".header-mobile nav")
+const nav = document.querySelector("header .mobile")
 
 botaoabrir.addEventListener('clik', abrirMenu)
 botaofechar.addEventListener('click', fecharMenu)
